4281d574872f0100816f2e0ebe2e9188e3b15daa
alee228 <deraldotera2@gmail.com>
2026-02-02 21:40:45 +0700

    selesai buat front end(demo)

---
+++ dashboard/urls.py
@@ -0,0 +1,12 @@
+from django.urls import path
+from . import views
+
+urlpatterns = [
+    path('', views.dashboard, name='dashboard'),
+    path('reservasi/', views.reservasi_list, name='reservasi'),
+    path('payments/', views.payments, name='payments'),
+    path('reports/', views.reports, name='reports'),
+    
+    # buat laporan bisa jalan
+    path('api/report-data/', views.api_report_data, name='api_report_data'),
+]
